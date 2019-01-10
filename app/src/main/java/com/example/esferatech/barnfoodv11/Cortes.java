ce5821fc2b6cc6beddda2a720183c0a5757e5003
etien-andres <andres.etien@gmail.com>
2019-01-09 20:29:40 -0600

    Agregado un nuevo dialogo con un calendario para el filtro de fechas

--- app/src/main/java/com/example/esferatech/barnfoodv11/Cortes.java
+++ app/src/main/java/com/example/esferatech/barnfoodv11/Cortes.java
@@ -1,13 +1,18 @@
 package com.example.esferatech.barnfoodv11;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -23,7 +28,7 @@ public class Cortes extends AppCompatActivity {
     GridView listaventas;
     Sqlitehelp helper;
     ImageButton back;
-    TextView date_today,grantotal;
+    TextView date_toda,grantotal;
     Date date;
     SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     String fechaformateada;
@@ -48,15 +53,22 @@ public class Cortes extends AppCompatActivity {
                 finish();
             }
         });
-        Estaticas.settitle(this,"Movimientos");
+        Estaticas.settitle(this,"Cortes");
         listaventas.setAdapter(new Adprventas(filtrarfecha()));
-        date_today=findViewById(R.id.date_actual);
+        //date_today=findViewById(R.id.date_actual);
 
 
         grantotal.setText(calcuar_gran_tota().toString());
+        final CustomDialogClass dialogo=new CustomDialogClass(this);
+        changefecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogo.show();
+            }
+        });
 
 
-        date_today.setText(solo_fecha);
+        changefecha.setText(solo_fecha);
 
 
     }
@@ -135,5 +147,87 @@ public class Cortes extends AppCompatActivity {
             str+=fechacompleta.charAt(i);
         }
         return str;
+    }
+
+    public class CustomDialogClass extends Dialog implements
+            android.view.View.OnClickListener {
+
+        public Activity c;
+        public Dialog d;
+        public Button yes, no;
+        CalendarView calendarView;
+        Date selected;
+        String fechaselct="";
+
+        public CustomDialogClass(Activity a) {
+            super(a);
+            this.c = a;
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.custom_dialog_1);
+            calendarView=findViewById(R.id.calendarView);
+
+            yes = (Button) findViewById(R.id.btn_yes);
+            no = (Button) findViewById(R.id.btn_no);
+            yes.setOnClickListener(this);
+            no.setOnClickListener(this);
+            Date date=new Date();
+            calendarView.setMaxDate(date.getTime());
+            calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                @Override
+                public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                    if (dayOfMonth<10){
+                        if ((month+1)<10){
+                            fechaselct="0"+dayOfMonth+"/"+"0"+(month+1)+"/"+""+year;
+                        }
+                        else {
+                            fechaselct="0"+dayOfMonth+"/"+""+(month+1)+"/"+""+year;
+
+                        }
+
+                    }
+                    else {
+                        if ((month+1)<10){
+                            fechaselct=""+dayOfMonth+"/"+"0"+(month+1)+"/"+""+year;
+                        }
+                        else {
+                            fechaselct=""+dayOfMonth+"/"+""+(month+1)+"/"+""+year;
+
+                        }
+                    }
+                }
+            });
+
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_yes:
+                    //c.finish();
+                    //long datel=calendarView.getDate();
+                   // Date date=new Date(datel);
+                    //solo_fecha=get_only_fecha(format.format(date));
+                    changefecha.setText(fechaselct);
+                    solo_fecha=fechaselct;
+                    listaventas.setAdapter(new Adprventas(filtrarfecha()));
+                    grantotal.setText(calcuar_gran_tota().toString());
+
+                    dismiss();
+                    //System.out.println(datel);
+                    break;
+                case R.id.btn_no:
+                    dismiss();
+                    break;
+                default:
+                    break;
+            }
+            dismiss();
+        }
     }
 }
